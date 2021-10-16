0b1a7793bc94ab59e3f93b958818d6155851b7d6
Facundo <facundotisserasorribas@gmail.com>
2021-10-16 11:49:20 -0300

    practica simple en React- aplicacion de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
-import React, { useRef, useState } from "react";
+import React, {Fragment, useRef, useState, useEffect } from "react";
+//el fragment destructura para el return, si no lo coloco, en el import tengo que ponerlo en el return pra enviar la vista
+import {v4 as uuidv4} from 'uuid';
 import { TodoList } from "./components/TodoList";
+
+const KEY = "todosApp.todos";
 
 export function App(){
     const [todos, setTodos] = useState([
-            {id: 1, taks: 'tarea 1', complete: false}
+            {id: 1, taks:"Tarea 1", complete: false}
         ]);
-        const todoTaskRef= useRef()
+        const todoTaskRef= useRef();
+
+        useEffect(()=>{
+            const storedTodos =JSON.parse(localStorage.getItem(KEY));
+            if (storedTodos) {
+                setTodos(storedTodos)
+            }
+        },[]);
+
+        useEffect(() => {
+            localStorage.setItem(KEY, JSON.stringify(todos));
+        }, [todos])
+
+        const toggleTodo= (id) =>{
+            const newTodos= [...todos];
+
+            const todo = newTodos.find((todo)=> todo.id === id);
+
+            todo.complete = !todo.complete;
+
+            setTodos(newTodos);
+        };
         
         const handlenTodoAdd = () =>{
-            const task = todoTaskRef.current.value;
-                if (task === '') return;
-                    setTodos()
+            const taks = todoTaskRef.current.value;
+                if (taks === '') return;
+            
+        setTodos((prevTodos) => {
+                return [...prevTodos, {id: uuidv4(), taks, complete: false}]
+            })
+            
+        todoTaskRef.current.value = null;         
+        }; 
+        const handleClearAll= ()=>{
+            const newTodos=  todos.filter((todo)=>!todo.complete);
+            setTodos(newTodos)
         }
-        
     return(
-        <React.Fragment>
-        
-        <TodoList todos={todos} />
-        <input ref={todoTaskRef} type="text" placeholder='Nueva tarea' />
-        <button onClick={handlenTodoAdd}>agregar</button>
-        <button>eliminar</button>
+        <Fragment>
+        <h1>TAREAS A REALIZAR</h1>
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        <input class="nuevaTarea" ref={todoTaskRef} type="text" placeholder='Nueva tarea' /><br />
+        <button class= "botones" onClick={handlenTodoAdd}>Agregar</button>
+        <button class= "botones" onClick={handleClearAll}>Eliminar</button>
+        <div class="tareasPendiente">Te quedan {todos.filter((todo)=> !todo.complete).length} tareas</div>
 
-        </React.Fragment>
+        </Fragment>
     )
 };
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-export function TodoItem({todo}) {
-    const {id,task, complete } = todo;
-    return (
-        <li>
-            {task}
-        </li>
-    )
+export function TodoItem({todo, toggleTodo}) {
+    const { id, taks, complete } = todo;
+    const handleTodoClick = () =>{
+        toggleTodo(id);
+    };
+    
+    return <li><input type="checkbox" checked={complete} onChange={handleTodoClick}/>{taks}</li>
+
+    
 }
